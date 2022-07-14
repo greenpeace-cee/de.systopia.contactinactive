53a8d613d0c654f935b16c6c2494f52415913197
Patrick Figel <pfigel@greenpeace.org>
2022-07-14 15:47:42 +0200

    GP-22919 Upgrade civix

--- contactinactive.php
+++ contactinactive.php
@@ -27,15 +27,6 @@ function contactinactive_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function contactinactive_civicrm_xmlMenu(&$files) {
-  _contactinactive_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -89,54 +80,6 @@ function contactinactive_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _contactinactive_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function contactinactive_civicrm_managed(&$entities) {
-  _contactinactive_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contactinactive_civicrm_caseTypes(&$caseTypes) {
-  _contactinactive_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function contactinactive_civicrm_angularModules(&$angularModules) {
-  _contactinactive_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function contactinactive_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _contactinactive_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 function contactinactive_civicrm_summaryActions(&$actions, $contactId) {
   $actions['contactinactive'] = [
     'title'  => 'Set to Inactive',
@@ -155,4 +98,13 @@ function contactinactive_civicrm_searchTasks( $objectName, &$tasks ){
       'class' => 'CRM_Contactinactive_Form_Set'
     ];
   }
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function contactinactive_civicrm_entityTypes(&$entityTypes) {
+  _contactinactive_civix_civicrm_entityTypes($entityTypes);
 }
