1e3c59bd6b623cec386628528d626fa6059af934
Patrick Figel <pfigel@greenpeace.org>
2024-09-11 20:02:47 +0200

    Upgrade civix

--- contactinactive.php
+++ contactinactive.php
@@ -36,48 +36,12 @@ function contactinactive_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function contactinactive_civicrm_postInstall() {
-  _contactinactive_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function contactinactive_civicrm_uninstall() {
-  _contactinactive_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function contactinactive_civicrm_enable() {
   _contactinactive_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function contactinactive_civicrm_disable() {
-  _contactinactive_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function contactinactive_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _contactinactive_civix_civicrm_upgrade($op, $queue);
 }
 
 function contactinactive_civicrm_summaryActions(&$actions, $contactId) {
@@ -98,13 +62,4 @@ function contactinactive_civicrm_searchTasks( $objectName, &$tasks ){
       'class' => 'CRM_Contactinactive_Form_Set'
     ];
   }
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function contactinactive_civicrm_entityTypes(&$entityTypes) {
-  _contactinactive_civix_civicrm_entityTypes($entityTypes);
 }
